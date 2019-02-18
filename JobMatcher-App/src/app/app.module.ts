0f0f82891707456d654eabc555a4a2d97798783e
Thomas <xumingk1@gmail.com>
2019-02-18 22:11:09 +1100

    Add material design and app component

--- JobMatcher-App/src/app/app.module.ts
+++ JobMatcher-App/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatIconModule, MatInputModule,
+  MatMenuModule, MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+import { JobsComponent } from './jobs/jobs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    JobsComponent
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
+    MatMenuModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
