7e34ff45333a34e5dbfc239769f912e58d40d944
Thomas <xumingk1@gmail.com>
2019-02-19 17:04:36 +1100

    Add the job component switch to real backend service call

--- JobMatcher-App/src/app/jobs/jobs.component.ts
+++ JobMatcher-App/src/app/jobs/jobs.component.ts
@@ -12,25 +12,9 @@ export class JobsComponent implements OnInit {
 
   matchedJobs$: Observable<MatchedJob[]>;
 
-  constructor(private jobService: JobService) {
-    this.matchedJobs$ = of([{
-      jobId: 1,
-      company: 'JobAdder',
-      name: 'Full stack developer',
-      matchedCandidates: [
-        {
-          candidateId: 1,
-          name: 'Thomas',
-          matchedScore: 399,
-          matchedSkills: ['Angular', 'AspnetCore']
-        }
-      ]
-    }]);
-
-  }
+  constructor(private jobService: JobService) {}
 
   ngOnInit() {
-    // this.matchedJobs$ = this.jobService.GetJobsWithMatchedCandidates();
-    // this.jobService.GetJobsWithMatchedCandidates();
+    this.matchedJobs$ = this.jobService.GetJobsWithMatchedCandidates();
   }
 }
